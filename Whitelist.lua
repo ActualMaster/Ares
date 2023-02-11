a9dc3267b466294628c833d01e434995e4ea8c4d
RealMaster <119766152+ActualMaster@users.noreply.github.com>
2023-02-11 13:39:56 -0600

    Delete Whitelist.lua

--- Whitelist.lua
+++
@@ -1,9 +0,0 @@
-local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
-
-local whitelist = {
-  ["966043A9-29B4-414C-8C0E-82E8CCD5D654"] = true
-}
-
-local blacklist = {
-  ["00000000-0000-0000-0000-000000000000"] = true
-}
